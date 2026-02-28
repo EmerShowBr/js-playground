e83edb5fbf4a46959e9a6c550cf210114c6ee15c
Emersom Feix <139271716+EmerShowBr@users.noreply.github.com>
2026-02-28 00:36:57 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # js-playground
+
 Um espaço interativo para praticar e experimentar conceitos de JavaScript. Contendo exemplos, exercícios e pequenos projetos voltados para o desenvolvimento de habilidades na linguagem
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home () {
-    return <h1>"These pretzels are making me thirsty" </h1>
+function Home() {
+  return <h1>"These pretzels are making me thirsty" </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("ue");
 }
 
 export default Home;
